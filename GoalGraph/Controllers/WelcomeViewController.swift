476e6e5b62a290578e9062c9c8468378a71505eb
Jake Garcia <bballwiz4@gmail.com>
2019-11-13 16:41:21 -0800

    Restart setup of welcome view.

---
+++ GoalGraph/Controllers/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  GoalGraph
+//
+//  Created by Jake Garcia on 11/13/19.
+//  Copyright © 2019 Jake Garcia. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var viewBottomLayoutConstraint: NSLayoutConstraint!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
